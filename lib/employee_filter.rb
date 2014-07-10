98479b2c3da246d74780d81e814870a5e9eb3970
Jordan Dale <Jordan.T.Dale@gmail.com>
2014-07-10 09:56:23 -0600

    improved version of code

--- lib/employee_filter.rb
+++ lib/employee_filter.rb
@@ -6,24 +6,11 @@ class EmployeeFilter
     @array = array
   end
 
-  def array
-    @array
-  end
-
   def started_before_2006
-    date = Date.new(2006, 1, 1)
-    employees = []
-    counter = 0
-    array.each do
-      if array[counter].include?(array[counter][:start_date < date])
-        employees << array[counter][:first_name]
-      end
-      counter += 1
+    @array.select do |em|
+      em[:start_year].year < 2006
     end
-        employees.downcase
-  end
 
-  def all_with_start_date
-    people = [[]]
+
+    end
   end
-end
